64c8c7252be75d331c26619674ce3fdcf0179bb5
freeeki <freeeki@gmail.com>
2018-07-18 08:07:54 +0200

    some notes about scale

--- examples/equilibrium_ortho_qind.py
+++ examples/equilibrium_ortho_qind.py
@@ -9,11 +9,12 @@ import compas
 import compas_ags
 import compas_tna
 
-from compas.utilities import XFunc
-
 from compas_tna.diagrams import FormDiagram
 from compas_tna.diagrams import ForceDiagram
 
+from compas_ags.ags import identify_dof
+
+from compas_tna.equilibrium import horizontal
 from compas_tna.equilibrium import vertical_from_qind
 
 from compas_tna.viewers import FormViewer
@@ -25,12 +26,11 @@ __license__   = 'MIT License'
 __email__     = 'vanmelet@ethz.ch'
 
 
-__all__ = []
-
-
-def identify_dof(form):
-    f = XFunc('compas_ags.ags.graphstatics.identify_dof_xfunc')
-    return f(form.to_data())
+# the boundary conditions should be set such that the fore diagram has only closed convex faces
+# all internal and external forces (loads and reactions) should eb included in the form diagram
+# for a fixed pattern, equilibrium can be computed by controlling the independent edges
+# or by a parallellisation procedure involving the force diagram
+# in the former case, the force diagram can be constructed from the force densities of the form diagram
 
 
 form = FormDiagram.from_obj(compas.get('lines.obj'))
@@ -43,12 +43,21 @@ form.set_edges_attribute('is_edge', False, keys=form.edges_on_boundary())
 k, m, ind = identify_dof(form)
 
 for u, v in ind:
-    form.set_edge_attributes((u, v), ('is_ind', 'q'), (True, random.choice(range(1, 10))))
+    form.set_edge_attributes((u, v), ('is_ind', 'q'), (True, random.choice(range(1, 5))))
 
-vertical_from_qind(form, density=0.6)
+e = len(list(form.edges_where({'is_edge': True})))
+l = sum(form.edge_length(u, v) for u, v in form.edges_where({'is_edge': True})) / e
+d = (200) ** 0.5 / e
+rho = 0.5 * l * d
 
+# if no convergence after 100 iterations
+# increase the scale
 
-form.to_json(os.path.join(compas_tna.DATA, 'notrhino.json'))
+# check zmax => 0.1 * d < zmax < 0.5 * d
+
+print(rho)
+
+vertical_from_qind(form, density=d, kmax=200)
 
 
 viewer = FormViewer(form, figsize=(10, 7))
@@ -61,7 +70,8 @@ viewer.draw_vertices(
 
 viewer.draw_edges(
     keys=list(form.edges_where({'is_edge': True})),
-    width={key: 3.0 for key in form.edges_where({'is_ind': True})})
+    width={key: 3.0 for key in form.edges_where({'is_ind': True})},
+    text={(u, v): "{:.1f}".format(attr['q']) for u, v, attr in form.edges_where({'is_edge': True}, True)})
 
 viewer.draw_faces()
 viewer.show()
