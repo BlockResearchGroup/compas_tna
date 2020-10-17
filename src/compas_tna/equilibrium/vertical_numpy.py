68fea029d89c42d78f6d6397ffc740a52a951266
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2020-10-17 17:11:56 +0200

    update kwarg name to fit latest compas

--- src/compas_tna/equilibrium/vertical_numpy.py
+++ src/compas_tna/equilibrium/vertical_numpy.py
@@ -87,7 +87,7 @@ def vertical_from_zmax(form, zmax, kmax=100, xtol=1e-2, rtol=1e-3, density=1.0, 
     p = array(form.vertices_attributes(('px', 'py', 'pz')), dtype=float64)
 
     edges = list(form.edges_where({'_is_edge': True}))
-    q = array(form.edges_attribute('q', keys=edges), dtype=float64).reshape((-1, 1))
+    q = array(form.edges_attribute('q', edges=edges), dtype=float64).reshape((-1, 1))
     edges = [(k_i[u], k_i[v]) for u, v in edges]
 
     C = connectivity_matrix(edges, 'csr')
@@ -205,7 +205,7 @@ def vertical_from_q(form, scale=1.0, density=1.0, kmax=100, tol=1e-3, display=Fa
     p = array(form.vertices_attributes(('px', 'py', 'pz')), dtype=float64)
 
     edges = list(form.edges_where({'_is_edge': True}))
-    q = array(form.edges_attribute('q', keys=edges), dtype=float64).reshape((-1, 1))
+    q = array(form.edges_attribute('q', edges=edges), dtype=float64).reshape((-1, 1))
     edges = [(k_i[u], k_i[v]) for u, v in edges]
 
     C = connectivity_matrix(edges, 'csr')
