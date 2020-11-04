9da15dcf84b82730d2dc9177fc5c66020c9a5022
Li <leochen1990@gmail.com>
2020-11-04 19:03:09 +0100

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name='compas_tna',
-    version='0.1.3',
+    version='0.1.4',
     description='COMPAS package for Thrust Network Analysis',
     long_description=long_description,
     long_description_content_type='text/markdown',
