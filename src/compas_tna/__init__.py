31299e073624e8bbd5406a0a80008a5618e0384d
tomvanmele <tom.v.mele@gmail.com>
2024-03-01 00:39:06 +0100

    Bump version to 0.4.0

--- src/compas_tna/__init__.py
+++ src/compas_tna/__init__.py
@@ -8,7 +8,7 @@ __copyright__ = "ETH Zurich - Block Research Group"
 __license__ = "MIT License"
 __email__ = "tom.v.mele@gmail.com"
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 
 HERE = os.path.dirname(__file__)
